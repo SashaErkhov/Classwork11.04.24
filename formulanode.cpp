bce7d2f0bfb8e1a3f71f2592d1dfb09a9b22fe15
Aleksandr Erkhov <sasori654654@gmail.com>
2024-04-11 13:46:49 +0300

    start

---
+++ formulanode.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sasor on 11.04.2024.
+//
+
+#include "formulanode.h"
---
+++ formulanode.h
@@ -0,0 +1,13 @@
+//
+// Created by sasor on 11.04.2024.
+//
+
+#ifndef CALC_FORMULANODE_H
+#define CALC_FORMULANODE_H
+
+class FormulaNode
+{
+
+};
+
+#endif //CALC_FORMULANODE_H
